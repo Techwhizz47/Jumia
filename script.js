dcacf3454e39b87cb055a92e8208e2c5c72970d1
Techwhizz47 <allanndungu47@gmail.com>
2024-05-26 18:05:38 +0300

    sticky navbar

--- script.js
+++ script.js
@@ -97,3 +97,20 @@ selectElem.addEventListener("click", () => {
 
 
 });
+
+//sticky navbar
+window.onscroll = function() {myFunction()};
+// Get the navbar
+const navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+const sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove the sticky class when you leave the scroll position.
+function myFunction() {
+  if (window.scrollY  >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
